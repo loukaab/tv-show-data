878076e56b996917e6bae2c58055ae05e00c6e37
loukababic <louka@babicconsulting.com>
2026-08-06 22:09:04 -0400

    plotting number of seasons vs vote average

--- tv-analysis.py
+++ tv-analysis.py
@@ -1,8 +1,17 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
+
+# "id","name","number_of_seasons","number_of_episodes","original_language","vote_count","vote_average","overview","adult",
+# "backdrop_path","first_air_date","last_air_date","homepage","in_production","original_name","popularity","poster_path","type",
+# "status","tagline","genres","created_by","languages","networks","origin_country","spoken_languages","production_companies",
+# "production_countries","episode_run_time"
 
 shows = pd.read_csv("TMDB_tv_dataset_v3.csv")
 
-print(f"{shows.describe()}")
+eshows = shows[shows["vote_count"] > 1000]
+#seasons = eshows[eshows["number_of_seasons"]]
+#ratings = eshows[eshows["vote_average"]]
 
+plt.scatter(eshows["number_of_seasons"], eshows["vote_average"], alpha=0.3)
+plt.show()
