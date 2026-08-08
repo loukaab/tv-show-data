5868296c799f3bd3827674234e3652aa494ee2a0
loukababic <louka@babicconsulting.com>
2026-08-08 11:30:33 -0400

    linear regression, averaged show ratings

--- tv-analysis.py
+++ tv-analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import linregress
 
 # "id","name","number_of_seasons","number_of_episodes","original_language","vote_count","vote_average","overview","adult",
 # "backdrop_path","first_air_date","last_air_date","homepage","in_production","original_name","popularity","poster_path","type",
@@ -10,12 +11,29 @@ import numpy as np
 shows = pd.read_csv("TMDB_tv_dataset_v3.csv")
 
 eshows = shows[shows["vote_count"] > 1000]
-#seasons = eshows[eshows["number_of_seasons"]]
-#ratings = eshows[eshows["vote_average"]]
+eshows = eshows.dropna(subset=["vote_count", "number_of_seasons"])
+seasonsXrating = eshows[["name", "number_of_seasons","vote_average"]]
 
-plt.scatter(eshows["number_of_seasons"], eshows["vote_average"], alpha=0.3)
-plt.xlabel("Number of Seasons")
-plt.ylabel("Average Rating")
-plt.title("Average TV Series Rating vs Number of Seasons")
+season_mean_and_count = seasonsXrating.groupby("number_of_seasons", as_index=False)["vote_average"].agg(["mean", "count"])
+
+# Linear regression
+reg = linregress(season_mean_and_count["number_of_seasons"], season_mean_and_count["mean"])
+stats = (
+    f"y = {reg.slope:.3f}x + {reg.intercept:.3f}\n"
+    f"R^2 = {reg.rvalue ** 2:.3f}\n"
+    f"P-Value = {reg.pvalue:.3f}"
+)
+
+# Make figure
+fig, ax = plt.subplots()
+
+ax.plot(season_mean_and_count["number_of_seasons"], reg.intercept +  reg.slope * season_mean_and_count["number_of_seasons"], color="red", linestyle="dashed")
+ax.text(0.5, 0.95, stats)
+
+ax.scatter(season_mean_and_count["number_of_seasons"], season_mean_and_count["mean"], alpha=0.3)
+ax.set_xlabel("Number of Seasons")
+ax.set_ylim(0, 10)
+ax.set_ylabel("Average Rating Across All Shows")
+ax.set_title("Average TV Series Rating vs Number of Seasons")
 
 plt.show()
